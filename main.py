7f98faba17336dfdcfca692d8a380a8dce56876b
Roboman01851 <roboman01851@gmail.com>
2025-02-21 19:20:39 -0500

    first

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class AndroidKivyApp(App):
         self.label = Label(text="Hello from Android!", font_size='24sp')
         layout.add_widget(self.label)
 
-        # Add a button
+        # Add a butto
         button = Button(text="Press Me", size_hint=(0.6, 0.2), pos_hint={'center_x': 0.5})
         button.bind(on_press=self.on_button_press)
         layout.add_widget(button)
