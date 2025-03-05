905a6214053c9e6a9aa061c36c73d25d4b6da42e
Roboman01851 <roboman01851@gmail.com>
2025-03-05 00:20:15 -0500

    file directory setup

--- main.py
+++ main.py
@@ -15,13 +15,64 @@ class AndroidKivyApp(App):
 
         # Add a button
         button = Button(text="This is a test button", size_hint=(0.6, 0.2), pos_hint={'center_x': 0.5})
-        button.bind(on_press=self.on_button_press)
+        button.bind(on_press=self.on_button_press_test)
         layout.add_widget(button)
 
         return layout
 
-    def on_button_press(self, instance):
+    def on_button_press_test(self, instance):
         self.label.text = "FUCK YOU LOL"
+
+    ## MAIN MENU BUTTONS ##
+
+    def on_button_press_party(self, instance):
+        return 0
+
+    def on_button_press_chara(self, instance):
+        return 0
+
+    def on_button_press_archive(self, instance):
+        return 0
+
+    def on_button_press_academ(self, instance):
+        return 0
+
+    def on_button_press_gacha(self, instance):
+        return 0
+
+    def on_button_press_fuse(self, instance):
+        return 0
+
+    def on_button_press_calc(self, instance):
+        return 0
+
+    ###########################################
+
+    ## EXTRA ##
+
+    def on_button_press_spirit(self, instance):
+        return 0
+
+    def on_button_press_lore(self, instance):
+        return 0
+
+    def on_button_press_skll(self, instance):
+        return 0
+
+    def on_button_press_beast(self, instance):
+        return 0
+
+    def on_button_press_chara(self, instance):
+        return 0
+
+    #############################################
+
+
+
+
+
+
+
 
 # Run the app
 if __name__ == "__main__":
