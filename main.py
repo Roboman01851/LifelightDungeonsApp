1f62985f9b54d400c4545146f84cee974ba8c50e
Roboman01851 <roboman01851@gmail.com>
2025-03-04 19:49:37 -0500

    file directory setup

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ class AndroidKivyApp(App):
         layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
 
         # Add a label
-        self.label = Label(text="Hello from Android!", font_size='24sp')
+        self.label = Label(text="Press button for message.", font_size='24sp')
         layout.add_widget(self.label)
 
-        # Add a butto
-        button = Button(text="Press Me", size_hint=(0.6, 0.2), pos_hint={'center_x': 0.5})
+        # Add a button
+        button = Button(text="This is a test button", size_hint=(0.6, 0.2), pos_hint={'center_x': 0.5})
         button.bind(on_press=self.on_button_press)
         layout.add_widget(button)
 
         return layout
 
     def on_button_press(self, instance):
-        self.label.text = "Button Pressed on Android!"
+        self.label.text = "FUCK YOU LOL"
 
 # Run the app
 if __name__ == "__main__":
