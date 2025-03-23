21eb3edf08810a2d2b2bcc82fa0d881f74aceb05
Roboman01851 <roboman01851@gmail.com>
2025-03-22 22:12:00 -0400

    Issues With Button Functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from json import *
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.button import Button, ButtonBehavior
 from kivy.uix.image import Image
@@ -9,7 +10,6 @@ from kivy.resources import resource_find
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.core.audio import SoundLoader
-import kivy
 
 class AndroidKivyApp(App):
     def build(self):
@@ -24,44 +24,40 @@ class AndroidKivyApp(App):
         scrolling_buttons_menu.add_widget(button_grid_menu)
         mainmenu.add_widget(scrolling_buttons_menu)
 
-        self.debug_label = Label(text="Debug output here", size_hint=(1, 0.1))
-
-        scrolling_buttons_menu.add_widget(self.debug_label)
-
         # Party Button
-        button_party = Button(background_normal='appassets/button_imgs/button_party.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_party = Button(background_normal='appassets/button_imgs/button_party.png',size_hint=(1, 1), pos_hint={'center_x': 0.5}) # Full Scale
         button_party.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_party)
+        button_grid_menu.add_widget(button_party)
 
         # Character Button
-        button_chara = Button(background_normal='appassets/button_imgs/button_characters.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_chara = Button(background_normal='appassets/button_imgs/button_characters.png',size_hint=(0.5, None),height=Window.width * 0.5 * (328 / 1017), pos_hint={'center_x': 0.5}) # 50% height and 50% width with respect to image resolution
         button_chara.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_chara)
+        button_grid_menu.add_widget(button_chara)
 
         # Archive Button
-        button_archive = Button(background_normal='appassets/button_imgs/button_archives.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_archive = Button(background_normal='appassets/button_imgs/button_archives.png',size_hint=(None, None), pos_hint={'center_x': 0.5}) # No Set with or height
         button_archive.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_archive)
+        button_grid_menu.add_widget(button_archive)
 
         # Academic Button
-        button_academ = Button(background_normal='appassets/button_imgs/button_academics.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_academ = Button(background_normal='appassets/button_imgs/button_academics.png',size_hint=(None, None), height=Window.width * 0.5 * (328 / 1017), width=Window.height * 0.5 * (328 / 1017), pos_hint={'center_x': 0.5}) # 50% hieght and with with respect to image resolution
         button_academ.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_academ)
+        button_grid_menu.add_widget(button_academ)
 
         # Gacha Button
-        button_gacha = Button(background_normal='appassets/button_imgs/button_gacha.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_gacha = Button(background_normal='appassets/button_imgs/button_gacha.png',size_hint=(1, None), pos_hint={'center_x': 0.5})
         button_gacha.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_gacha)
+        button_grid_menu.add_widget(button_gacha)
 
         # Fusion Button
-        button_fusion = Button(background_normal='appassets/button_imgs/button_fusion.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_fusion = Button(background_normal='appassets/button_imgs/button_fusion.png',size_hint=(1, None), pos_hint={'center_x': 0.5})
         button_fusion.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_fusion)
+        button_grid_menu.add_widget(button_fusion)
 
         # Calculator Button
-        button_calculator = Button(background_normal='appassets/button_imgs/button_calc.png',size_hint=(0.6, 0.4), pos_hint={'center_x': 0.5})
+        button_calculator = Button(background_normal='appassets/button_imgs/button_calc.png',size_hint=(1, None), pos_hint={'center_x': 0.5})
         button_calculator.bind(on_press=self.on_button_press_test)
-        scrolling_buttons_menu.add_widget(button_calculator)
+        button_grid_menu.add_widget(button_calculator)
 
 
 
